7699118c436e90dc8f855780e817a595430a87dd
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2026-04-20 11:55:52 +0700

    refactor(billing): tidy create stripe customer service for readability

--- enterprise/app/services/enterprise/billing/create_stripe_customer_service.rb
+++ enterprise/app/services/enterprise/billing/create_stripe_customer_service.rb
@@ -20,12 +20,10 @@ class Enterprise::Billing::CreateStripeCustomerService
   private
 
   def prepare_customer_id
-    customer_id = account.custom_attributes['stripe_customer_id']
-    if customer_id.blank?
-      customer = Stripe::Customer.create({ name: account.name, email: billing_email })
-      customer_id = customer.id
-    end
-    customer_id
+    existing_id = account.custom_attributes['stripe_customer_id']
+    return existing_id if existing_id.present?
+
+    Stripe::Customer.create(name: account.name, email: billing_email).id
   end
 
   def default_quantity
@@ -37,30 +35,22 @@ class Enterprise::Billing::CreateStripeCustomerService
   end
 
   def default_plan
-    installation_config = InstallationConfig.find_by(name: 'CHATWOOT_CLOUD_PLANS')
-    @default_plan ||= installation_config.value.first
+    @default_plan ||= InstallationConfig.find_by(name: 'CHATWOOT_CLOUD_PLANS').value.first
   end
 
   def price_id
-    price_ids = default_plan['price_ids']
-    price_ids.first
+    default_plan['price_ids'].first
   end
 
   def active_subscription
     stripe_customer_id = account.custom_attributes['stripe_customer_id']
     return nil if stripe_customer_id.blank?
 
-    Stripe::Subscription.list(
-      {
-        customer: stripe_customer_id,
-        status: 'active',
-        limit: 1
-      }
-    ).data.first
+    Stripe::Subscription.list(customer: stripe_customer_id, status: 'active', limit: 1).data.first
   end
 
   def default_plan_subscription?(subscription)
-    Array(default_plan['price_ids']).include?(subscription['plan']['id'])
+    default_plan['price_ids'].include?(subscription['plan']['id'])
   end
 
   def build_custom_attributes(customer_id, subscription)
